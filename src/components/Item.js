ab11d4a924a1bd6aee9255708f66d51b512b6d4d
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-05-06 18:59:53 -0400

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+   const [mode, setMode] = useState(true);
+   const btnClass = mode ? '' : 'in-cart';
+   function handleItem() {
+      setMode((mode) => !mode);
+   }
+   return (
+      <li className="">
+         <span>{name}</span>
+         <span className="category">{category}</span>
+         <button className={btnClass} onClick={handleItem}>
+            {mode ? 'Add to Cart' : 'Remove from Cart'}
+         </button>
+      </li>
+   );
 }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,33 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from 'react';
+import Item from './Item';
 
 function ShoppingList({ items }) {
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+   const [selectedCategory, setSelectedCategory] = useState('All');
+
+   const filteredItems = items.filter((item) =>
+      selectedCategory === 'All' ? true : item.category === selectedCategory
+   );
+
+   function handleSelect(e) {
+      setSelectedCategory(e.target.value);
+   }
+   return (
+      <div className="ShoppingList">
+         <div className="Filter">
+            <select name="filter" onChange={(e) => handleSelect(e)}>
+               <option value="All">Filter by category</option>
+               <option value="Produce">Produce</option>
+               <option value="Dairy">Dairy</option>
+               <option value="Dessert">Dessert</option>
+            </select>
+         </div>
+         <ul className="Items">
+            {filteredItems.map((item) => (
+               <Item key={item.id} name={item.name} category={item.category} />
+            ))}
+         </ul>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+   );
 }
 
 export default ShoppingList;
